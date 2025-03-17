edee3443643c280497f7bd8ad1673ec8e4baa605
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2025-03-17 23:54:12 +0900

    Feat: 과제 작성

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ const App = () => {
         // TODO: 카트에 상품을 추가하는 함수를 완성하세요.
         // 카트에 상품이 8개 이상이면 alert를 띄워
         // 더 이상 상품을 추가할 수 없다고 알려주세요.
-
-        setCart([...cart, product]);
     };
 
     return (
