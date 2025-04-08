9fbf9db09b0c7acc435003a8f6da1f2ea895844b
yongarfram <yongar2002@gmail.com>
2025-04-08 14:29:05 +0900

    장바구니 완성하기

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ const App = () => {
     const [cart, setCart] = useState([]);
 
     const addToCart = (product) => {
+        if (cart.length >= 8) {
+            alert('더이상 상품을 추가할 수 없습니다.');
+            return;
+        }
+        setCart([...cart, product]);
         // TODO: 카트에 상품을 추가하는 함수를 완성하세요.
         // 상품은 배열객체로 이루어져 있습니다.
         // 카트에 상품이 8개 이상이면 alert를 띄워
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,23 +5,34 @@ const Cart = ({ cart, setCart }) => {
 
     //TODO: 장바구니를 열고 닫는 함수를 완성하세요.
     //setIsOpen 함수를 사용하여 State값을 업데이트하세요.
-    const toggleCart = () => {};
+    const toggleCart = () => {
+        if (cart.length > 0) {
+            setIsOpen(!isOpen);
+        }
+    };
 
     //TODO: cart에 담긴 상품의 총 가격을 계산하세요.
     //reduce 함수를 사용하여 총 가격을 계산하세요.
-    const totalPrice = 0;
+    const totalPrice = cart.reduce((rec, item) => {
+        return rec + item.price;
+    }, 0);
 
     //TODO: (선택 과제) 상품 가격에 서식을 지정하세요.
     //상품 가격을 3자리마다 콤마(,)를 찍어주는 함수를 작성하세요.
-    //예시) 1000000 => 1,000,000
+    //예시) 1000000 => 1,000,000ㅔ
     //이 함수의 사용법은 totalPriceFormatted(가격) 입니다.
     //HINT : https://shape-coding.tistory.com/72
-    const totalPriceFormatted = () => {};
+    const totalPriceFormatted = () => {
+        return totalPrice.toLocaleString();
+    };
 
     //TODO: 결제하기 버튼 구현하기
     //결제하기 버튼을 눌렀을 때 alert를 띄우고
     //장바구니를 비워주세요.
-    const handlePurchase = () => {};
+    const handlePurchase = () => {
+        alert('결재가 진행됩니다.');
+        setCart([]);
+    };
 
     return (
         <div className="cart">
